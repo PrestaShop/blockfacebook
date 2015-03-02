12329d6111caca092350c499eb18255ad82cc146
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-03-02 11:09:59 +0100

    // Version updated to v1.3.4

--- blockfacebook.php
+++ blockfacebook.php
@@ -33,7 +33,7 @@ class BlockFacebook extends Module
 	{
 		$this->name = 'blockfacebook';
 		$this->tab = 'front_office_features';
-		$this->version = '1.3.3';
+		$this->version = '1.3.4';
 		$this->author = 'PrestaShop';
 
 		$this->bootstrap = true;
